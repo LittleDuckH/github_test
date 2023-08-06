0b95a60ec6a26f7146893a463c3e5de288fb8ab9
LittleDuckH <3213203570@qq.com>
2023-08-06 14:03:13 +0800

    main

---
+++ backup/cmake_test002/src/t2.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World from t2 test!\n");
+    return 0;
+}
---
+++ backup/cmake_test003/lib/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.hpp"
+
+void HelloFunc()
+{
+    printf("Hello World\n");
+}
